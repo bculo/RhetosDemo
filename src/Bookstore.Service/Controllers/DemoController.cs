608d33721b239bd2f8e91e40ccf2bc2668e2e402
bculo <culobozo@gmail.com>
2022-10-04 10:03:27 +0200

    Flat Rhetos DSL syntax added

---
+++ src/Bookstore.Service/Controllers/DemoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rhetos.Processing;
+using Rhetos;
+using Rhetos.Processing.DefaultCommands;
+using Rhetos.Host.AspNet.RestApi.Filters;
+
+namespace Bookstore.Service.Controllers
+{
+    [ApiController]
+    [Route("Demo/[action]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly IProcessingEngine processingEngine;
+        private readonly IUnitOfWork unitOfWork;
+
+        public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork)
+        {
+            this.processingEngine = processingEngine.Value;
+            this.unitOfWork = unitOfWork.Value;
+        }
+
+        [HttpGet]
+        public string ReadBooks()
+        {
+            var readCommandInfo = new ReadCommandInfo { DataSource = "Bookstore.Book", ReadTotalCount = true };
+            var result = processingEngine.Execute(readCommandInfo);
+            return $"{result.TotalCount} books.";
+        }
+
+        [HttpGet]
+        [ServiceFilter(typeof(ApiCommitOnSuccessFilter))]
+        public string WriteBook()
+        {
+            var newBook = new Bookstore.Book { Title = "NewBook" };
+            var saveCommandInfo = new SaveEntityCommandInfo { Entity = "Bookstore.Book", DataToInsert = new[] { newBook } };
+            processingEngine.Execute(saveCommandInfo);
+            return "1 book inserted.";
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            //var updateCommand = new Command
+
+            return "Test";
+        }
+    }
+}
--- src/Bookstore.Service/Program.cs
+++ src/Bookstore.Service/Program.cs
@@ -8,18 +8,29 @@ builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHos
         .UseBuilderLogProviderFromHost(serviceProvider)
         .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
     .AddAspNetCoreIdentityUser()
-    .AddHostLogging();
+    .AddHostLogging()
+    .AddDashboard()
+    .AddRestApi(o =>
+    {
+        o.BaseRoute = "rest";
+        o.GroupNameMapper = (conceptInfo, controller, oldName) => "v1";
+    });
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(type => type.ToString())); 
 
 var app = builder.Build();
+
+app.UseRhetosRestApi();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapRhetosDashboard();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
